035e2a1a183d7655a93edc8745e6c596eddd0893
Harsh47200 <sharmaharsh472000@gmail.com>
2025-09-05 19:51:45 +0530

    success

--- frontend/src/component/ContactSection/index.jsx
+++ frontend/src/component/ContactSection/index.jsx
@@ -13,7 +13,7 @@ const ContactSection = () => {
     phoneNumber: '',
     details: ''
   });
-const API_URL = "http://myjavaapp-env.eba-gia9yifj.eu-north-1.elasticbeanstalk.com/";
+const API_URL = "http://jcp-api-env.eba-m32tj9rb.eu-north-1.elasticbeanstalk.com/";
   const handleRequestService = async () => {
     setIsLoading(prev => ({ ...prev, sms: true }));
     setMessages(prev => ({ ...prev, sms: "" }));
